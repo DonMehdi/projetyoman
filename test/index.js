e71132e4c822e04c8a2f71ec598740d15902b00a
DonMehdi <mehdi.koraichi@gmail.com>
2015-12-01 16:11:09 +0100

    correction erreur

--- test/index.js
+++ test/index.js
@@ -1,11 +1,8 @@
 import assert from 'assert';
 import projetyoman from '../lib';
 
- 
-
 describe('projetyoman', function () {
   it('should have a version number!', function () {
-    assert(typeof catatonicCircle.VERSION !== 'undefined', 'The Project should have a VERSION, whatever the actual version.');
+    assert(typeof projetyoman.VERSION !== 'undefined', 'The Project should have a VERSION, whatever the actual version.');
   });
 });
-
