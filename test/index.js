45a5745eefffe127efdc4c8b3ded89fe8d68b06c
DonMehdi <mehdi.koraichi@gmail.com>
2015-12-01 17:10:52 +0100

    utilisation de push

--- test/index.js
+++ test/index.js
@@ -1,11 +1,11 @@
-import assert from 'assert';
+//import assert from 'assert';
 import projetyoman from '../lib';
 import { should } from 'chai';
 
 should(); // Modifies objects prototype to include the 'should' property
-
 describe('projetyoman', function () {
   it('should have a version number!', function () {
-    assert(typeof projetyoman.VERSION !== 'undefined', 'The Project should have a VERSION, whatever the actual version.');
+    projetyoman.should.have.property('VERSION');
+    /*assert(typeof projetyoman.VERSION !== 'undefined', 'The Project should have a VERSION, whatever the actual version.');*/
   });
 });
